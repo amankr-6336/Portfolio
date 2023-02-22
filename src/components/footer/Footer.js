cefbff05b3d03853bb9acb1c1efc37b68e0f9354
amankr.6336 <amankr.17709418@gmail.com>
2023-02-22 15:37:10 +0530

    second

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,11 +14,11 @@ function Footer() {
         <div className="footerinner">
             <div className="footername"><h4>Aman Kumar</h4></div>
             <div className="footericon">
-                    <a href=""> <AiFillGithub/></a>
-                    <a href=""> <AiFillLinkedin/> </a>
-                    <a href=""> <RxTwitterLogo/></a>
-                    <a href=""> <BsInstagram/></a>
-                    <a href=""> <BsFacebook/></a>
+                    <a href="https://github.com/amankr-6336?tab=repositories"> <AiFillGithub/></a>
+                    <a href="https://www.linkedin.com/in/aman-kumar-269275170/"> <AiFillLinkedin/> </a>
+                    <a href="https://twitter.com/"> <RxTwitterLogo/></a>
+                    <a href="https://www.instagram.com/"> <BsInstagram/></a>
+                    <a href="https://www.facebook.com/"> <BsFacebook/></a>
             </div>
             
         </div>
