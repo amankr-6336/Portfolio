b459d54fe3b591b82c0b93f7ad980a5b415da595
amankr.6336 <amankr.17709418@gmail.com>
2023-02-22 15:30:13 +0530

    second

--- src/components/introduction/Introduction.js
+++ src/components/introduction/Introduction.js
@@ -20,11 +20,11 @@ function Introduction() {
         <div className="container">
             <div className="left">
                 <div className="socialmedia">
-                    <a href="https://github.com/amankr-6336?tab=repositories" target="_blank"> <AiFillGithub/></a>
-                    <a href="https://www.linkedin.com/in/aman-kumar-269275170/" target="_blank"> <AiFillLinkedin/> </a>
-                    <a href=""> <RxTwitterLogo/></a>
-                    <a href=""> <BsInstagram/></a>
-                    <a href=""> <BsFacebook/></a>
+                    <a href="https://github.com/amankr-6336?tab=repositories" > <AiFillGithub/></a>
+                    <a href="https://www.linkedin.com/in/aman-kumar-269275170/"> <AiFillLinkedin/> </a>
+                    <a href="https://twitter.com/"> <RxTwitterLogo/></a>
+                    <a href="https://www.instagram.com/"> <BsInstagram/></a>
+                    <a href="https://www.facebook.com/"> <BsFacebook/></a>
                 </div>
                 <div className="name">
                   <h2>Hello, &nbsp;</h2>
